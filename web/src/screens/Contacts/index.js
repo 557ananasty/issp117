1f5cb1efdda59be4469f3885b482aabb8d85f533
Dasha Glushneva <glushnevadasha@mail.ru>
2020-10-27 09:54:17 +0300

    add contacts page

---
+++ web/src/screens/Contacts/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "./index.css";
+import { contacts } from "../../util/data";
+
+export default class Contacts extends Component {
+  render() {
+    return (
+      <div id='contacts' className='page fon'>
+        <div>
+          <img src={require("../../assets/image/map.svg")} height={200} />
+        </div>
+        <div>
+          <div>{`e-mail : ${contacts.email}`}</div>
+          <div>{`телефон : ${contacts.phone}`}</div>
+          <div>{`адрес : ${contacts.adress}`}</div>
+        </div>
+      </div>
+    );
+  }
+}
--- web/src/screens/Home/index.js
+++ web/src/screens/Home/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import HomePage from "./HomePage";
-// import Contacts from "../Contacts";
+import Contacts from "../Contacts";
 
 export default function Home() {
   return (
     <div>
       <HomePage />
-      {/* <Contacts /> */}
+      <Contacts />
     </div>
   );
 }
